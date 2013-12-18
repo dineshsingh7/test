dcae661514ae599edb0e568572847cbbe5771b43
Harish <harish.chouhan@infobeans.com>
2013-12-18 15:58:17 +0530

    InfoBeans test
    Commited Test Project

---
+++ TestProject/TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Tarun Seera on 18/12/13.
+//  Copyright (c) 2013 InfoBeans Systems India Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
